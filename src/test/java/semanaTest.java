9fc1c2e9f559156d3019b858d6936e590b7bf32e
Emagmunioz <emagmunioz@gmail.com>
2025-02-02 19:04:28 +0100

    testCrearListaDias is undefined for the type semanaTest

--- src/test/java/semanaTest.java
+++ src/test/java/semanaTest.java
@@ -5,5 +5,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+
 public class semanaTest {
+    private semana gestor;
+    @BeforeEach
+    public void setUp() {
+        gestor = new semana();
+        gestor.crearListaDias();
+    }
+    @Test
+    public void testCrearListaDias() {
+        List<String> dias = gestor.obtenerDiasSemana();
+        assertThat(dias, hasSize(7));
+        assertThat(dias, containsInAnyOrder("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"));
+    }  
 }

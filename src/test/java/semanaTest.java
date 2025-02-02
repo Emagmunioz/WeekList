3d11ade85f9e475fcd16ecd67a29d833276a7917
Emagmunioz <emagmunioz@gmail.com>
2025-02-02 18:54:59 +0100

    The import org.hamcrest cannot be resolved

---
+++ src/test/java/semanaTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class semanaTest {
+}

98e5b37028e561abf70fd018d3e172bf04ad8b08
Emagmunioz <emagmunioz@gmail.com>
2025-02-02 19:19:09 +0100

    obtenerLargoLista implemented

--- src/main/java/semana.java
+++ src/main/java/semana.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 public class semana {
+    private List<String> diasSemana;
 
+    public semana() {
+        this.diasSemana = new ArrayList<>();
+    }
+public void crearListaDias() {
+    diasSemana.add("Lunes");
+    diasSemana.add("Martes");
+    diasSemana.add("Miércoles");
+    diasSemana.add("Jueves");
+    diasSemana.add("Viernes");
+    diasSemana.add("Sábado");
+    diasSemana.add("Domingo");
+    }   
+
+public List<String> obtenerDiasSemana() {
+    return diasSemana;
+}
+public int obtenerLargoLista() {
+    return diasSemana.size();
+}
 }
